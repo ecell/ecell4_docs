55cf1b0a505cc88f359be4a7a47c9e400adf3541
kaizu <kwaizu@gmail.com>
2020-04-23 17:27:59 +0900

    Change theme

--- en/conf.py
+++ en/conf.py
@@ -50,8 +50,9 @@ exclude_patterns = ['_build', '**.ipynb_checkpoints']
 pygments_style = 'sphinx'
 
 import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme = "sphinx_rtd_theme"
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'sphinx_typlog_theme'
 
 html_static_path = ['_static']
 
