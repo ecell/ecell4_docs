9eb7f49db7f78336c19486e4f9c65d12e3201ff4
kaizu <kwaizu@gmail.com>
2020-05-11 10:03:00 +0900

    Run all notebooks

--- misc/main.sh
+++ misc/main.sh
@@ -5,5 +5,33 @@ ls .
 cd ecell4_docs
 ls .
 export ARTIFACTS_PATH=/artifacts
+cd en
 
-papermill en/top.ipynb ${ARTIFACTS_PATH}/top.ipynb
+papermill top.ipynb ${ARTIFACTS_PATH}/top.ipynb
+
+mkdir -p ${ARTIFACTS_PATH}/tutorials
+cd tutorials
+for filename in *.ipynb; do
+    echo ${filename}
+    name=`basename "$filename"`
+    papermill ${name} ${ARTIFACTS_PATH}/tutorials/${name}
+done
+cd ..
+
+mkdir -p ${ARTIFACTS_PATH}/tests
+cd tests
+for filename in *.ipynb; do
+    echo ${filename}
+    name=`basename "$filename"`
+    papermill ${name} ${ARTIFACTS_PATH}/tests/${name}
+done
+cd ..
+
+mkdir -p ${ARTIFACTS_PATH}/examples
+cd examples
+for filename in *.ipynb; do
+    echo ${filename}
+    name=`basename "$filename"`
+    papermill ${name} ${ARTIFACTS_PATH}/examples/${name}
+done
+cd ..
